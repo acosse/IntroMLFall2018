d9f3a28efe04534edfe42f9be6a21d7185c0c501
Augustin Cosse <augustinmcosse@gmail.com>
2018-10-09 13:32:48 +0200

    Add files via upload

---
+++ Exercice2/ConvolutionalNeuralNet.py
@@ -0,0 +1,5 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+df_train = pd.read_csv('../input/train.csv')
+df_test = pd.read_csv('../input/test.csv')
